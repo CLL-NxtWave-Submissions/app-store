da741c1602a6ac0f7b6ad32434eb1902cec05805
Surya T N <suryatej.nim@yahoo.com>
2022-09-08 16:35:15 +0000

    Implemented the AppItem UI layout with required data and styling

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -1,1 +1,15 @@
-// Write your code here
+import './index.css'
+
+const AppItem = props => {
+  const {itemData} = props
+  const {appName, imageUrl} = itemData
+
+  return (
+    <li className="app-item-bg-container">
+      <img className="app-item-img" src={imageUrl} alt={appName} />
+      <p className="app-item-name">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
