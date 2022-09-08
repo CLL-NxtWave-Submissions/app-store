05ad23346b215f3719a52670f8a2dac895135ee6
Surya T N <suryatej.nim@yahoo.com>
2022-09-08 15:11:30 +0000

    Implemented TabItem component to render intended tab item UI with required CSS styling for JSX elements

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -1,1 +1,22 @@
-// Write your code here
+import './index.css'
+
+const TabItem = props => {
+  const {itemData, itemSelectHandler, isSelected} = props
+  const {tabId, displayText} = itemData
+
+  const itemStyling = isSelected ? 'tab-item selected-tab-item' : 'tab-item'
+
+  const itemClickHandler = () => {
+    itemSelectHandler(tabId)
+  }
+
+  return (
+    <li className={itemStyling} onClick={itemClickHandler}>
+      <button type="button" className="tab-item-button">
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
